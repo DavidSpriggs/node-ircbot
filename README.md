2a349826e8502f6ecc9f9c65682bba34b2ace1f4
David Spriggs <spriggs.d@gmail.com>
2013-09-04 23:17:20 -0500

    added mongodb persistence for karma

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ node-ircbot
 
 Node irc bot used in the #esri channel on freenode.
 
-Welcomes and tracks karma.
+Welcomes users to room and tracks karma in mongodb.
+
+Karma given via: term++
+
+Karma taken via: term--
+
+List all karma via: !kamra!
--- bot/bot.js
+++ bot/bot.js
@@ -15,6 +15,10 @@ var config = {
 
 // Get the lib
 var irc = require("irc");
+var mongojs = require('mongojs');
+
+// connect to mongodb
+var db = mongojs('ircBot1', ['karma']);
 
 // Create the bot
 var bot = new irc.Client(config.server, config.nick, config.options);
@@ -23,7 +27,7 @@ var bot = new irc.Client(config.server, config.nick, config.options);
 bot.addListener("join", function(chan, nick) {
 	// Welcome them in!
 	if (nick !== config.nick) {
-		bot.say(chan, nick + ": Welcome back! Feel free to ask any questions! Give props using karma: nick++ or nick-- or any term: work-- or weekend++");
+		bot.say(chan, nick + ": Welcome back! Feel free to ask any questions! Give props using karma: nick++ or any term: work-- or weekend++");
 	}
 });
 
@@ -36,28 +40,68 @@ var karma = {};
 bot.addListener('message', function(nick, chan, text, message) {
 	if (printAll.test(text)) {
 		bot.say(chan, 'Listing all karma:');
-		for (var i in karma) {
-			bot.say(chan, i + ' has ' + karma[i] + ' karma');
-		}
+		db.karma.find().sort({
+			term: 1
+		}, function(err, docs) {
+			docs.forEach(function(doc) {
+				bot.say(chan, doc.term + ' has ' + doc.karma + ' karma');
+			});
+		});
 	} else {
-		var p;
+		var term;
 		if (plusOne.test(text)) {
-			p = text.match(plusOne)[1];
-			if (karma[p] !== undefined) {
-				karma[p] = karma[p] + 1;
-			} else {
-				karma[p] = 1;
-			}
-			bot.say(chan, p + ' has ' + karma[p] + ' karma');
+			term = text.match(plusOne)[1];
+			db.karma.findOne({
+				term: term
+			}, function(err, doc) {
+				if (!doc) {
+					db.karma.save({
+						term: term,
+						karma: 1
+					});
+					bot.say(chan, term + ' has 1 karma');
+				} else {
+					db.karma.update({
+						term: term
+					}, {
+						$inc: {
+							karma: 1
+						}
+					}, {
+						multi: false
+					}, function() {
+						// the update is complete
+						bot.say(chan, term + ' has ' + (doc.karma + 1) + ' karma');
+					});
+				}
+			});
 		}
 		if (minusOne.test(text)) {
-			p = text.match(minusOne)[1];
-			if (karma[p] !== undefined) {
-				karma[p] = karma[p] - 1;
-			} else {
-				karma[p] = -1;
-			}
-			bot.say(chan, p + ' has ' + karma[p] + ' karma');
+			term = text.match(minusOne)[1];
+			db.karma.findOne({
+				term: term
+			}, function(err, doc) {
+				if (!doc) {
+					db.karma.save({
+						term: term,
+						karma: -1
+					});
+					bot.say(chan, term + ' has -1 karma');
+				} else {
+					db.karma.update({
+						term: term
+					}, {
+						$inc: {
+							karma: -1
+						}
+					}, {
+						multi: false
+					}, function() {
+						// the update is complete
+						bot.say(chan, term + ' has ' + (doc.karma - 1) + ' karma');
+					});
+				}
+			});
 		}
 	}
 });
